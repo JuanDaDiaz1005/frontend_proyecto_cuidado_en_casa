84fd95025f318ff4488aa1752fc00b083b3a6564
JuanDaDiaz1005 <juan.1701920068@ucaldas.edu.co>
2023-05-15 20:59:12 -0500

    Implementación de algunas rutas, se inicia con el desarrollo de la página de inicio, la barra de navegación, se define la paleta de colores a usar y se crean algunos componentes.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,83 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue';
+onMounted(() => {
+  const registrar = document.getElementById('registrar_desplegable');
+
+  registrar.addEventListener('click', () => {
+    const links= document.getElementById('registrar');
+    links.classList.toggle('registrar-menu');
+})
+})
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="nav-links">
+        <div id="links-principales">
+          <RouterLink to="/"><img class="logo-inicio" src="../src/assets/logo.svg" alt=""></RouterLink>
+          <RouterLink to="/about">Consultar paciente</RouterLink>
+          <div id="registrar_desplegable">
+            Registrar
+            <ul id="registrar" class="registrar-menu">
+              <li><RouterLink to="/about">Diagnóstico</RouterLink></li>
+              <li><RouterLink to="/about">Sugerencia de cuidado</RouterLink></li>
+            </ul>
+          </div>
+        </div>
+        <div id="link-login">
+          <RouterLink to="/login">Ingresar</RouterLink>  
+        </div>
+    </nav>
   </header>
 
-  <RouterView />
+  <body>
+    <RouterView/>
+  </body>
+  
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+.nav-links{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  background-color: var(--color-soft-blue);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.logo-inicio{
+  margin: 0;
+  width: 30px;
+  height: 30px;
+  padding-left: 5px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+ #links-principales{
+  display: flex;
+  flex-direction: row;
+  padding-left: 5px;
+  align-items: center;
+  
+ }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+ #link-login{
+  padding-right: 5px;
+ }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+.registrar-menu {
+  list-style: none;
+  display:none;
+  padding-left: 5px;
+ }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+ #registrar-desplegable{
+  cursor:pointer;
+  display: flex;
+  flex-direction: column;
+ }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
