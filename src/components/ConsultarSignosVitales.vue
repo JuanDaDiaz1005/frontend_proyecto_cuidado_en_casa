e78e43487ad36fac82fb5695ad888ecc70923e37
JuanDaDiaz1005 <juan.1701920068@ucaldas.edu.co>
2023-05-23 15:15:04 -0500

    Se añaden estilos al caso de uso consultar signos vitales

--- src/components/ConsultarSignosVitales.vue
+++ src/components/ConsultarSignosVitales.vue
@@ -5,87 +5,6 @@ const rightDate = ref('')
 const signosVitales = ref([])
 const validDateRange = ref(true)
 
-
-const temporalUseCase = [
-    {
-        "id": 41,
-        "fecha": "2022-01-01 08:00:00",
-        "nombre_signo": "Temperatura",
-        "unidad": "°C",
-        "valor": 120.0
-    },
-    {
-        "id": 51,
-        "fecha": "2022-01-02 08:00:00",
-        "nombre_signo": "Temperatura",
-        "unidad": "°C",
-        "valor": 90.0
-    },
-    {
-        "id": 42,
-        "fecha": "2022-01-01 08:30:00",
-        "nombre_signo": "Frecuencia cardíaca",
-        "unidad": "ppm",
-        "valor": 80.0
-    },
-    {
-        "id": 43,
-        "fecha": "2022-01-01 09:00:00",
-        "nombre_signo": "Presión arterial sis",
-        "unidad": "mmHg",
-        "valor": 16.0
-    },
-    {
-        "id": 44,
-        "fecha": "2022-01-01 09:30:00",
-        "nombre_signo": "Presión arterial dia",
-        "unidad": "mmHg",
-        "valor": 90.0
-    },
-    {
-        "id": 45,
-        "fecha": "2022-01-01 10:00:00",
-        "nombre_signo": "Frecuencia respirato",
-        "unidad": "rpm",
-        "valor": 70.0
-    },
-    {
-        "id": 46,
-        "fecha": "2022-01-01 10:30:00",
-        "nombre_signo": "Saturación de oxígen",
-        "unidad": "%",
-        "valor": 100.0
-    },
-    {
-        "id": 47,
-        "fecha": "2022-01-01 11:00:00",
-        "nombre_signo": "Glucemia",
-        "unidad": "mg/d",
-        "valor": 110.0
-    },
-    {
-        "id": 48,
-        "fecha": "2022-01-01 11:30:00",
-        "nombre_signo": "Peso",
-        "unidad": "kg",
-        "valor": 80.0
-    },
-    {
-        "id": 49,
-        "fecha": "2022-01-01 12:00:00",
-        "nombre_signo": "Altura",
-        "unidad": "cm",
-        "valor": 70.0
-    },
-    {
-        "id": 50,
-        "fecha": "2022-01-01 12:30:00",
-        "nombre_signo": "IMC",
-        "unidad": "kg/m",
-        "valor": 120.0
-    }
-]
-
 const getSignosVitales = () => {
 
     signosVitales.value = []
@@ -103,9 +22,21 @@ const getSignosVitales = () => {
 
     validDateRange.value = true
 
-    setTimeout(() => {
-        signosVitales.value = temporalUseCase
-    }, 1000);
+    const body = {
+        "id": 3,
+        "fecha_inicio": leftDateStr,
+        "fecha_fin": rightDateStr
+    }
+    console.log(body);
+    fetch('http://127.0.0.1:8000/signo_vital/consultar_signos_vitales', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+    .then(res => res.json())
+    .then(data => signosVitales.value = data)
 }
 
 const getDateTimeStr = (date) => {
@@ -116,17 +47,26 @@ const getDateTimeStr = (date) => {
 </script>
 
 <template>
+     <div v-if="signosVitales.length == 0" class="title">
+            <div>Ingrese el rango de fechas en el que desea conocer los signos vitales del paciente</div>
+     </div>
     <div class="inputs-container">
-        <input @change="getSignosVitales()" class="date" :class="validDateRange ? '' : 'dangerDate'" type="datetime-local"
+        <div>
+            <label>Fecha inicial:</label>
+            <input @change="getSignosVitales()" class="date" :class="validDateRange ? '' : 'dangerDate'" type="datetime-local"
             name="leftDate" id="leftDate" ref="leftDate">
-        <input @change="getSignosVitales()" class="date" :class="validDateRange ? '' : 'dangerDate'" type="datetime-local"
+        </div>
+        <div>
+            <label>Fecha inicial:</label>
+            <input @change="getSignosVitales()" class="date" :class="validDateRange ? '' : 'dangerDate'" type="datetime-local"
             name="rightDate" id="rightDate" ref="rightDate">
+        </div>
     </div>
-    <div>
-        <main v-if="signosVitales.length == 0">
-            <img src="..\assets\Loading_icon.gif" alt="">
-        </main>
-        <main v-else >
+    <div class="tabla_espacio">
+        <div v-if="signosVitales.length == 0" class="gif_carga">
+            <img src="..\assets/images/Loading_icon.gif" alt="">
+        </div>
+        <div v-else class="tabla">
             <table>
                 <thead>
                     <tr>
@@ -139,25 +79,27 @@ const getDateTimeStr = (date) => {
 
                 <tr v-for="signoVital, i in signosVitales" :key="i">
                     <td>{{ signoVital.fecha }}</td>
-                    <td>{{ signoVital.nombre_signo }}</td>
+                    <td>{{ signoVital.signo_vital.nombre_signo }}</td>
                     <td>{{ signoVital.valor }}</td>
-                    <td>{{ signoVital.unidad }}</td>
+                    <td>{{ signoVital.signo_vital.unidad }}</td>
                 </tr>
 
             </table>
-        </main>
+        </div>
 
     </div>
 </template>
 
 <style scoped>
 * {
-    font-size: 1.5rem;
+    font-family: var(--fuente-principal);
+    font-size: 15pt;
 }
 
 .inputs-container {
     display: flex;
     justify-content: space-around;
+    margin: 20px 0 0 0;
 }
 
 main {
@@ -168,7 +110,7 @@ main {
 
 table {
     border-collapse: collapse;
-    width: 90%;
+    width: 80%;
 }
 
 table thead {
@@ -176,14 +118,56 @@ table thead {
     color: var(--color-gray-900);
 }
 
-table td,
-table th {
-    border: 2px solid var(--color-gray-800);
-    padding: 8px;
+table th{
+    box-sizing: border-box;
+    padding: 15px 10px;
+    margin-bottom: 5px;
+    margin-right: 10px;
+}
+
+table td {
+    box-sizing: border-box;
+    /* border: none; */
+    border: 5px solid white;
+    background-color: var(--color-gray-100);
+    padding: 15px 10px;
+    margin-bottom: 5px;
+    margin-right: 10px;
 }
 
 .dangerDate {
     border-color: var(--color-danger);
 }
+
+.title{
+    display: flex;
+    justify-content:center ;
+    font-family: var(--fuente-principal);
+    margin: 20px 0px;
+}
+
+.gif_carga{
+    display: flex;
+    justify-content: center;
+    margin: 20px 0px;
+    height: 100px;
+}
+
+.tabla_espacio{
+    margin-top: 20px;
+    margin-bottom: 30px;
+}
+
+.tabla{
+    display:flex;
+    justify-content: center;
+}
+
+label{
+    font-family: var(--fuente-principal);
+    margin-right: 20px;
+}
 </style>
+
+
 
